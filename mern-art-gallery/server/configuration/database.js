b5db96bd4f6ac69abed81264d068559bd91cbd90
chinmayBKh <chinmay.bonde.cmaug25@gmail.com>
2025-11-02 23:33:41 +0530

    2nd small changes

--- mern-art-gallery/server/configuration/database.js
+++ mern-art-gallery/server/configuration/database.js
@@ -4,7 +4,7 @@ mongoose.set('strictQuery', true);
 
 export const connectDatabase = async () => {
     try {
-        const {connection} = await mongoose.connect(process.env.DATABASE_URI);
+        const {connection} = await mongoose.connect(process.env.DB_URI);
         console.log(`Database connected with ${connection.host}`);
     } catch (error) {
         console.log(error)
